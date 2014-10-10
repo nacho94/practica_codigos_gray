52bdb1d94422321abbfc6ac3d16e5b4a4d905ae8
Nacho <macmini@Mac-de-Nacho.local>
2014-10-10 18:55:04 +0200

    inicializador

--- Gray.java
+++ Gray.java
@@ -1,7 +1,20 @@
 public class Gray {
-	public static void main(string){
+	public static void main(String [] args){
 		System.out.println("Gray");
 
 	}
+	private static boolean [][] result;
 
+	private static void inicializar(int n) {
+		result = new boolean [2^n][n];
+	}
+
+	public static boolean[][] gray(int n,boolean [][] codigos) {
+		if(n >= 2) {
+
+		} else {
+			return codigos;
+		}
+		return null;
+	}
 }
